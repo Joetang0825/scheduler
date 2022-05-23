4dbbe703ebd34360fac95ab1fb42e02bfe8d9339
Joe Tang <joetang81@gmail.com>
2022-05-23 10:56:59 -0400

    implement showing appointments on the schedule

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,7 +5,6 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
-
 export default function Appointment(props) {
 
   return <article className="appointment">
