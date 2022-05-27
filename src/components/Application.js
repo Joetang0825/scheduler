61260fef3dcf88fb07da4558d1dffdba19d399d3
Joe Tang <joetang81@gmail.com>
2022-05-26 21:44:59 -0400

    implement logic to request the days through api call

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
+import axios from "axios";
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,6 +46,14 @@ const appointments = {
 };
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      console.log(response);
+      setDays(response.data);
+    });
+  }, []);
+
   const [day, setDay] = useState("Monday");
 
 
