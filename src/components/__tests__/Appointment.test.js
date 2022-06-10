720c8b9c030ebc63fe2f75b2fcb7fd5a26850a0d
Joe Tang <joetang81@gmail.com>
2022-06-10 15:42:03 -0400

    add first unit test for checking student name and interviewer

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
 
-import Appointment from "components/Application";
+import Appointment from "components/Appointment"
 
 describe("Appointment", () => {
   it("render without crashing", () => {
     render(<Appointment />);
   });
 });
+
